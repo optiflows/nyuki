02d3f422ef700b53a5978466590eba2e89278f6a
Ketouem <cyril.thomas@surycat.com>
2018-03-01 09:45:39 +0100

    Preventing accidental upload to pypi and store local package

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -1,12 +1,19 @@
-echo -e "[distutils]
-index-servers=pypi
+VERSION_TAG=$1
 
-[pypi]
-username = $PYPI_USER
-password = $PYPI_PASSWORD
-" > ~/.pypirc
+if [ ! -z $VERSION_TAG ]; then
+  echo -e "[distutils]
+  index-servers=pypi
 
-pipenv lock -r > requirements.txt
-echo $CIRCLE_TAG > VERSION.txt
-python setup.py sdist
-twine upload dist/nyuki-$CIRCLE_TAG.tar.gz
+  [pypi]
+  username = $PYPI_USER
+  password = $PYPI_PASSWORD
+  " > ~/.pypirc
+
+  pipenv lock -r > requirements.txt
+  echo $VERSION_TAG > VERSION.txt
+  python setup.py sdist
+  twine upload dist/nyuki-$VERSION_TAG.tar.gz
+else
+  echo "No VERSION_TAG defined, skipping packaging and upload."
+  exit 1;
+fi
