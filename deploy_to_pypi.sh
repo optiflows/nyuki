69bddb49eefff9dfeee7ee684562bc71dc6b81ea
Guillaume Desvé <guillaume.desve@surycat.com>
2018-03-15 15:39:31 +0100

    Update circle workflow (contexts)

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -1,18 +1,10 @@
 VERSION_TAG=$1
 
 if [ ! -z $VERSION_TAG ]; then
-  echo -e "[distutils]
-  index-servers=pypi
-
-  [pypi]
-  username = $PYPI_USER
-  password = $PYPI_PASSWORD
-  " > ~/.pypirc
-
   pipenv lock -r > requirements.txt
   echo $VERSION_TAG > VERSION.txt
   python setup.py sdist
-  twine upload dist/nyuki-$VERSION_TAG.tar.gz
+  twine upload -u $PYPI_USER -p $PYPI_PASSWORD dist/nyuki-$VERSION_TAG.tar.gz
 else
   echo "No VERSION_TAG defined, skipping packaging and upload."
   exit 1;
