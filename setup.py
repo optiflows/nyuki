c8d2e9bd711dffe070929a12bfa0765dfe007633
Guillaume Desvé <guillaume.desve@surycat.com>
2018-05-03 11:47:06 +0200

    Use pip's internal parse_requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
-def parse_requirements(filename):
-    lineiter = (line.strip() for line in open(filename))
-    return [
-        line for line in lineiter
-        if line and not line.startswith('#') and not line.startswith('-')
-    ]
-
+# Requirements
+install_reqs = parse_requirements('requirements.txt', session='dummy')
+reqs = [str(ir.req) for ir in install_reqs]
 
 try:
     with open('VERSION.txt', 'r') as v:
@@ -28,7 +28,7 @@ setup(
     author='Optiflows R&D',
     author_email='rand@surycat.com',
     version=version,
-    install_requires=parse_requirements('requirements.txt'),
+    install_requires=reqs,
     packages=find_packages(exclude=['tests']),
     license='Apache 2.0',
     classifiers=[
